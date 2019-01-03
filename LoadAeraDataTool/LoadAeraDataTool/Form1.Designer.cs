3fb26371a42427f09ebaf901705e7a8097d73ba1
CavanOrBxj <40481651+CavanOrBxj@users.noreply.github.com>
2019-01-03 10:01:11 +0800

    数据库加载区域码信息工具增加

---
+++ LoadAeraDataTool/LoadAeraDataTool/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace LoadAeraDataTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDbPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDbuser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfilename = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.txtDb);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtDbPass);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtDbuser);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(475, 198);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数据库设置";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(110, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 14);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "数 据 库";
+            // 
+            // txtDb
+            // 
+            this.txtDb.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDb.Location = new System.Drawing.Point(187, 76);
+            this.txtDb.Name = "txtDb";
+            this.txtDb.Size = new System.Drawing.Size(179, 23);
+            this.txtDb.TabIndex = 29;
+            this.txtDb.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(109, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 14);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "登录密码";
+            // 
+            // txtDbPass
+            // 
+            this.txtDbPass.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDbPass.Location = new System.Drawing.Point(187, 146);
+            this.txtDbPass.Name = "txtDbPass";
+            this.txtDbPass.Size = new System.Drawing.Size(179, 23);
+            this.txtDbPass.TabIndex = 27;
+            this.txtDbPass.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(110, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 14);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "用 户 名";
+            // 
+            // txtDbuser
+            // 
+            this.txtDbuser.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDbuser.Location = new System.Drawing.Point(187, 111);
+            this.txtDbuser.Name = "txtDbuser";
+            this.txtDbuser.Size = new System.Drawing.Size(179, 23);
+            this.txtDbuser.TabIndex = 25;
+            this.txtDbuser.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(110, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 14);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "服 务 器";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtServer.Location = new System.Drawing.Point(187, 41);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(179, 23);
+            this.txtServer.TabIndex = 23;
+            this.txtServer.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtfilename);
+            this.groupBox2.Location = new System.Drawing.Point(24, 216);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(475, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "文件加载";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 31);
+            this.button1.TabIndex = 31;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(109, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 14);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "区域码文件";
+            // 
+            // txtfilename
+            // 
+            this.txtfilename.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtfilename.Location = new System.Drawing.Point(187, 39);
+            this.txtfilename.Name = "txtfilename";
+            this.txtfilename.Size = new System.Drawing.Size(179, 23);
+            this.txtfilename.TabIndex = 29;
+            this.txtfilename.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(352, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "创建AeraCodeNation表并导入数据（不使用）";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(97, 427);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(352, 54);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "开始替换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 564);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "区域码导入工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtDb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDbPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDbuser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfilename;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ LoadAeraDataTool/LoadAeraDataTool/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoadAeraDataTool
+{
+    public partial class Form1 : Form
+    {
+
+        string filepath = "";
+
+        public static dbAccess dba;
+
+        public Dictionary<string, string> GB_CODE_Dic;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtfilename.Text = openFileDialog1.FileName;
+            }
+           
+        }
+
+
+        //获取数据库连接字符串
+        private String GetConnectString()
+        {
+            string sReturn;
+            string sPass;
+            sPass = txtDbPass.Text.Trim();
+            if (sPass == "")
+                sPass = "tuners2012";
+            sReturn = "server = " + txtServer.Text.Trim() +
+                   ";UID = " + txtDbuser.Text.Trim() +
+                    ";Password = " + sPass +
+                     ";DataBase = " + txtDb.Text.Trim() + ";"
+                     + "MultipleActiveResultSets=True";
+
+            return sReturn;
+        }
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtServer.Text!="" || txtDb.Text!="" || txtDbuser.Text!="" || txtDbPass.Text!="")
+                {
+
+
+                    dba = new dbAccess();
+                    dba.conn.ConnectionString = GetConnectString();
+
+                    if (dba.OpenConn())
+                    {
+                        //先判断有误表 AeraCodeNation  没有的话先创建
+                        String tableNameStr = "select count(1) from sysobjects where name = 'AeraCodeNation'";
+
+                        SqlCommand cmd = new SqlCommand(tableNameStr, dba.conn);
+                        int result = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (result == 0)
+                        {
+                            //未创建  开始创建表 AeraCodeNation
+                            if (dba.conn.State==ConnectionState.Open)
+                            {
+                                dba.conn.Close();
+                            }
+                            dba.OpenConn();
+                            string sql222 = "CREATE TABLE AeraCodeNation([Code][varchar](50) COLLATE Chinese_PRC_CI_AS NOT NULL,[Name] [varchar] (50) COLLATE Chinese_PRC_CI_AS NULL,CONSTRAINT[PK_AeraCodeNation] PRIMARY KEY CLUSTERED([Code] ASC)WITH(IGNORE_DUP_KEY = OFF) ON[PRIMARY]) ON[PRIMARY]";
+                            cmd = new SqlCommand(sql222,dba.conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("数据库信息不能为空！");
+                    return;
+                }
+
+
+                if (txtfilename.Text!="")
+                {
+                    string[] lines = System.IO.File.ReadAllLines(txtfilename.Text);
+
+                    if (lines.Length>0)
+                    {
+                        foreach (var item in lines)
+                        {
+                            string[] tmp = item.Split('#');
+                            string code = tmp[1];
+                            string name = tmp[0];
+                            string InfoValueStr = "insert into AeraCodeNation values('" + code + "','" + name + "')";
+                            dba.UpdateDbBySQL(InfoValueStr);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("文件名不能为空！");
+                }
+
+                MessageBox.Show("数据导入完成");
+              
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtServer.Text != "" || txtDb.Text != "" || txtDbuser.Text != "" || txtDbPass.Text != "")
+                {
+
+
+                    dba = new dbAccess();
+                    dba.conn.ConnectionString = GetConnectString();
+
+                    if (dba.OpenConn())
+                    {
+                        //判断表中有无数据列GB_CODE
+                        string sql223 = "select* from   syscolumns where   id = object_id('Organization')   and name = 'GB_CODE'";
+                        SqlCommand cmd = new SqlCommand(sql223, dba.conn);
+                       object pp= cmd.ExecuteScalar();
+                        if (pp == null)
+                        {
+                            //没有数据列，往表中Organization插入列 GB_CODE
+                            string SQL11 = "ALTER TABLE Organization ADD GB_CODE [varchar](255) COLLATE Chinese_PRC_CI_AS NULL";
+                            cmd = new SqlCommand(SQL11, dba.conn);
+                            cmd.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            //已经有数据列
+                            Console.WriteLine("GB_CODE已经存在");
+                        }
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("数据库信息不能为空！");
+                    return;
+                }
+
+
+                if (txtfilename.Text != "")
+                {
+                    string[] lines = System.IO.File.ReadAllLines(txtfilename.Text);
+
+                  string   MediaSql = "select  ORG_ID,ORG_DETAIL,ORG_CODEA from Organization";
+                   DataTable dtMedia = dba.getQueryInfoBySQL(MediaSql);
+
+
+                    if (lines.Length > 0)
+                    {
+                        foreach (var item in lines)
+                        {
+                            string[] tmp = item.Split('#');
+                            string code = tmp[1];
+                            string name = tmp[0].Trim();
+                            string selectString = "";
+                            if (code.Substring(2, 10) == "0000000000" || code.Substring(4, 8) == "00000000")
+                            {
+                                //省或者市
+                                selectString = string.Format("ORG_DETAIL='{0}'", name);
+                            }
+                            else
+                            {
+                                string tunerscode = "P" + code .Substring(0,2)+ "Q"+ code.Substring(2,2) + "C"+ code.Substring(4,2);
+                                selectString = string.Format("ORG_DETAIL='{0}' and ORG_CODEA like '{1}%'", name, tunerscode);
+                            }
+
+                           
+
+                         
+                            DataRow[] dr = dtMedia.Select(selectString);
+                            if (dr!=null && dr.Length>0)
+                            {
+                                if (dr.Length == 1)
+                                {
+                                    string InfoValueStr = "UPDATE Organization set GB_CODE ='" + code + "' where ORG_ID='" + dr[0][0].ToString() + "'";
+                                    dba.UpdateDbBySQL(InfoValueStr);
+                                }
+                            }
+                           
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("文件名不能为空！");
+                }
+
+                MessageBox.Show("数据导入完成");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
